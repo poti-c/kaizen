e28b944b6d06ce0980bcea23bac085dbcbdeeb7c
poti-c <poti@eng.cmu.ac.th>
2026-08-02 09:16:25 +0000

    Deploying to gh-pages from @ poti-c/kaizen@e3b4cc268401cc5ad84f525bae94688516f2dcb5 🚀

--- assets/search-legacy-Dyhh3xEf.js
+++
@@ -1,8 +0,0 @@
-System.register(["./index-legacy-DJOUd1QE.js"],function(e,t){"use strict";var c;return{setters:[function(e){c=e.a3}],execute:function(){
-/**
-       * @license lucide-react v0.462.0 - ISC
-       *
-       * This source code is licensed under the ISC license.
-       * See the LICENSE file in the root directory of this source tree.
-       */
-e("S",c("Search",[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]))}}});
