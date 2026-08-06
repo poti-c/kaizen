a6e732ba3ecc37a99e792304cc2b53513b977da1
poti-c <poti@eng.cmu.ac.th>
2026-08-06 14:00:10 +0000

    Deploying to gh-pages from @ poti-c/kaizen@6c76c5abd5b4999cdee4ae1056408093b44411ae 🚀

--- assets/ban-legacy-D76bwNsA.js
+++
@@ -1,8 +0,0 @@
-System.register(["./index-legacy-DDOXlDpA.js"],function(e,t){"use strict";var c;return{setters:[function(e){c=e.a3}],execute:function(){
-/**
-       * @license lucide-react v0.462.0 - ISC
-       *
-       * This source code is licensed under the ISC license.
-       * See the LICENSE file in the root directory of this source tree.
-       */
-e("B",c("Ban",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m4.9 4.9 14.2 14.2",key:"1m5liu"}]]))}}});
